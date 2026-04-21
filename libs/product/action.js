60696224a38c59113baf5f2a9ef4744e1840a844
aidilprmdta <sekaiiprojectmv@gmail.com>
2026-04-21 23:48:51 +0700

    🔥 feat – menambahkan function update product

--- libs/product/action.js
+++ libs/product/action.js
@@ -25,3 +25,17 @@ export async function createProduct(productData) {
         throw new Error("Failed to create product");
     }
 }
+
+export async function updateProduct(id, productData) {
+    try{
+        const { name, price, stock } = productData;
+        const [result] = await pool.query(
+            "UPDATE products SET name = ?, stock = ? WHERE id =",
+            [name, price, stock, id],
+        );
+        return result.affectedRows > 0;
+    } catch (error) {
+        console.error("Error updating product:", error);
+        throw new Error("Failed to update product");
+    }
+}   
