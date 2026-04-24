37289766aa0ebc2882c22e34cf21f73b9b344460
aidilprmdta <sekaiiprojectmv@gmail.com>
2026-04-24 14:29:00 +0700

    ⚡ fix – memperbaiki bug login

--- app/(auth)/login/page.jsx
+++ app/(auth)/login/page.jsx
@@ -4,7 +4,7 @@ export default function LoginPage() {
     return (
         <main className="min-h-screen flex items-center justify-center bg-gray-200">
             <div className="bg-white p-8 rounded-lg shadow-md w-96">
-                <h1 className="text-2xl font-bold mb-6 text-center text-blue 600">
+                <h1 className="text-2xl font-bold mb-6 text-center text-blue-600">
                     Login Maju Jaya
                 </h1>
 
